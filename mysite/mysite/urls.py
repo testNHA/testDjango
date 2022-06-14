e46de42d6454b056d9a9a75faa475a2598c7edd8
testNHA <i.martens@nha.nl>
2022-06-14 12:37:21 +0200

    toevoegen untracked files

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('admin/', admin.site.urls),
     path('', include('blog.urls')),
 ]
