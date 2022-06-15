f594b5ecbcd8890024c61595518da8c5b398e5c8
testNHA <i.martens@nha.nl>
2022-06-15 16:45:05 +0200

    toevoegingen

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -21,6 +21,7 @@ def post_nieuw(request):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return redirect('post_detail', pk=post.pk)
+            return redirect('post_inhoud', pk=post.pk)
         else:
-            print("Er ging iets mis!")
+            form = PostForm()
+        return render(request, 'blog/post_edit.html', {'form': form}
